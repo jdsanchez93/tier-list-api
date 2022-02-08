5354cb7b058b43622d342bc1d95405e34abb22ab
jd <jdsanchez2011@gmail.com>
2022-02-08 00:54:08 -0700

    configure connection string

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<TierListDbContext>(
     dbContextOptions => dbContextOptions
-        .UseMySql("Server=localhost;Port=3306;Database=TierList;Uid=root;Pwd=example;", new MySqlServerVersion(new Version(8, 0, 28)))
+        .UseMySql(builder.Configuration.GetConnectionString("Mysql"), new MySqlServerVersion(new Version(8, 0, 28)))
         // The following three options help with debugging, but should
         // be changed or removed for production.
         .LogTo(Console.WriteLine, LogLevel.Information)
